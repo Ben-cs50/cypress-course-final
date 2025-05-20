96b65091d6b33b40cefe19304aac276bf88e33da
Ben-cs50 <benedictodhiambo49@gmail.com>
2025-05-20 12:52:36 +0300

    Added changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,20 @@
+
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
+
+  reporter: 'cypress-mochawesome-reporter',
+
   e2e: {
     baseUrl: "http://localhost:3000",
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+
+      require('cypress-mochawesome-reporter/plugin')(on);
+
     },
+
   },
 
-  component: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
-  },
 });
